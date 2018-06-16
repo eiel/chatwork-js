9e6f1c0cea815bd0dd596251879d7946be3c0657
Tomohiko Himura <eiel.hal@gmail.com>
2018-06-16 22:52:38 +0900

    style(eslint): code format

--- packages/chatwork-api/index.js
+++ packages/chatwork-api/index.js
@@ -1,34 +1,45 @@
 // @flow
-import axios from 'axios'
-import { stringify as qs } from 'qs'
+import axios from 'axios';
+import { stringify as qs } from 'qs';
 
-const buildBody = (body: ?Object) => body ? { body: qs(body) } : {}
-const buildAuthHeader = (token: string): { 'X-ChatWorkToken': string } => ({ 'X-ChatWorkToken': token })
+const buildBody = (body: ?Object) => (body ? { body: qs(body) } : {});
+const buildAuthHeader = (token: string): { 'X-ChatWorkToken': string } => ({
+  'X-ChatWorkToken': token,
+});
 
 type Request = {
-  room_id: number,
+  roomId: number,
   body: string,
-}
+};
 type Token = {
-  token: string
-}
-type MessageID = string
+  token: string,
+};
+type MessageID = string;
 type Response = {
   message_id: MessageID,
-}
+};
 /*
  import { postRoomMessage } from './index'
  postRoomMessage({
-  room_id: 84100351,
+  roomId: roomId,
   body: 'test',
   token: CHATWORK_ACCESS_TOKEN
 })
 */
-export const postRoomMessage = ({room_id, body, token}: Request & Token): Promise<{data: Response, headers?: {[string]: string}}> =>
+export const postRoomMessage = ({
+  roomId,
+  body,
+  token,
+}: Request & Token): Promise<{
+  data: Response,
+  headers?: { [string]: string },
+}> =>
   axios({
     method: 'post',
-    url: `/rooms/${room_id}/messages`,
+    url: `/rooms/${roomId}/messages`,
     baseURL: 'https://api.chatwork.com/v2',
     data: { ...buildBody({ body }) }.body,
     headers: buildAuthHeader(token),
-  })
+  });
+
+export default { postRoomMessage };
