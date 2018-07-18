39b37b1c7894a521fc05d36565add5f24eef0ac8
Tomohiko Himura <eiel.hal@gmail.com>
2018-07-18 22:40:10 +0900

    feat: override getToken for simple-oaauth2
    avoid url escape client_id and client_secret

--- packages/koa-chatwork/src/simple-oauth2.js
+++ packages/koa-chatwork/src/simple-oauth2.js
@@ -1,4 +1,6 @@
 import SimpleOAuth2 from 'simple-oauth2';
+import querystring from 'querystring';
+import Wreck from 'wreck';
 import {
   tokenHost,
   tokenPath,
@@ -14,10 +16,36 @@ const auth = {
   authorizePath,
 };
 
-export const createOAuth2 = (client: Client) =>
-  SimpleOAuth2.create({
+const defaultHeaders = {
+  Accept: 'application/json',
+};
+const wreck = Wreck.defaults({ baseUrl: tokenHost, headers: defaultHeaders });
+
+/* replace original getToken */
+const getToken = ({ id, secret }: Client) => (params) => {
+  const credentials = `${id}:${secret}`;
+  const token = Buffer.from(credentials).toString('base64');
+  const Authorization = `Basic ${token}`;
+  const payload = querystring.stringify({
+    grant_type: 'authorization_code',
+    ...params,
+  });
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization,
+  };
+  return wreck
+    .post(auth.tokenPath, { headers, payload })
+    .then((result) => JSON.parse(result.payload.toString()));
+};
+
+export const createOAuth2 = (client: Client) => {
+  const oauth2 = SimpleOAuth2.create({
     auth,
     client,
   });
+  oauth2.authorizationCode.getToken = getToken(client);
+  return oauth2;
+};
 
 export default createOAuth2;
