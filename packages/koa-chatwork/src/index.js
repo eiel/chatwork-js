bf6ac4a25a2ed81401a04ebd56b92ab31dbed64a
Tomohiko Himura <eiel.hal@gmail.com>
2018-07-18 22:42:28 +0900

    feat: compose CallbackMiddleware

--- packages/koa-chatwork/src/index.js
+++ packages/koa-chatwork/src/index.js
@@ -1,6 +1,7 @@
 // @flow
 import type { Middleware } from 'koa';
 import { createAuthMiddleware } from './createAuthMiddleware';
+import { createCallbackMiddleware } from './createCallbackMiddleware';
 import { createOAuth2 } from './simple-oauth2';
 import type { Client } from './client';
 
@@ -19,9 +20,16 @@ const createKoaChatwork = (_options: Options): Middleware => {
   const options = { ..._options, ...defaultOptions };
   const oauth2 = createOAuth2(options.client);
   const AuthMiddleware = createAuthMiddleware({ scope: options.scope, oauth2 });
+  const CallbackMiddleware = createCallbackMiddleware({
+    scope: options.scope,
+    oauth2,
+  });
   return (ctx, next) => {
     if (ctx.path === options.signinPath) {
       return AuthMiddleware(ctx, async () => {});
+    }
+    if (ctx.path === '/chatwork/callback') {
+      return CallbackMiddleware(ctx, next);
     }
     return next();
   };
